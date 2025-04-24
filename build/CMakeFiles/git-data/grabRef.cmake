768497cdb1a0fa53858c4f3aefd8c1bfabaff027
mariem benhassine <mariem.benhassine79@gmail.com>
2025-04-24 11:14:48 +0200

    component

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "/home/mariem/ili9341_display_driver/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "/home/mariem/esp-idf/.git")
+set(GIT_DIR "/home/mariem/ili9341_display_driver/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
